9b1c2c989d65d5d80e041d41d319a7b5d49a1f53
ferchojoshua <40898768+ferchojoshua@users.noreply.github.com>
2024-07-01 15:54:45 -0600

    Add files via upload

--- src/services/ProductsApi.jsx
+++ src/services/ProductsApi.jsx
@@ -189,3 +189,32 @@ export const getAllStoresKardexAsync = async (token, data) => {
   }
   return result;
 };
+
+
+
+export const updateProductrecallAsync = async (token, data) => {
+  const result = { statusResponse: true, data: [], error: null };
+  let service = `${controller}UpdaterecallProduct`;
+  const authAxios = axios.create({
+    baseURL: service,
+    withCredentials: false,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  try {
+    await authAxios.post(service , data).then((resp) => {
+      if (resp.status <= 200 && resp.status >= 299) {
+        result.statusResponse = false;
+        result.error = resp.title;
+      } else {
+        result.statusResponse = true;
+        result.data = resp.data;
+      }
+    });
+  } catch (error) {
+    result.statusResponse = false;
+    result.error = error;
+  }
+  return result;
+};
